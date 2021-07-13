7649d157b3b5a47be0cd505cecfc24d149a523b4
eli <zilog87@gmail.com>
2021-07-14 03:57:17 +0700

    edit file main.py menambahkan file requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-HOla
+import requests
+print("hello world")
+
+try:
+    r = requests.get('https://google.com')
+    print(r.status_code)
+    if r.status_code == 200:
+        print((r.text))
+except Exception as e:
+    print('ada error', e)
