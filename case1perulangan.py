a917369b54dc1ee8937a36168a0c95d1471eef12
eli <zilog87@gmail.com>
2021-09-11 22:58:16 +0700

    sekuensial dan percabangan

---
+++ case1perulangan.py
@@ -0,0 +1,20 @@
+print('Ibu berkata, "pergi ke toko"')
+print('Budi menjawab, "Baik, apa yg harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beli satu botol susu"')
+print("maka budi berangkat ke toko")
+print("dan mulai berbelanja")
+
+jumlah_botol_susu = 89
+jumlah_telor = 99
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+    if jumlah_telor > 0:
+        print("budi membeli 6 telur")
+    else:
+        print("budi tidak membeli telur")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah dan melaporkan hasil belanjanya")
